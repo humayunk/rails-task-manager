1e1fe18afa6cb6e2286eafe01d9940c79371970d
humayun khan <humayun.n.k@gmail.com>
2024-05-21 17:02:49 -0400

    attempted challenge

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # See all
+  get "tasks", to: "tasks#index"
+  # Create one
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  # Read one
+  get "tasks/:id", to: "tasks#show", as: :task
+  # Update one
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  # Delete task
+  delete "tasks/:id", to: "tasks#destroy"
 end
